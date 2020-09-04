d2f89ef912b68ac2b74bf014c6eb7755abaeb09c
Temak <makindeoluwatimilehin@gmail.com>
2020-09-04 22:12:03 +0100

    db user update

--- blog-app/config/config.php
+++ blog-app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'https://wejapa.elitefashion.com.ng/blog-app/');
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'temak');
+	define('DB_USER', 'allround_temak');
 	define('DB_PASS', 'temak_password');
 	define('DB_NAME', 'allround_blogapp');
