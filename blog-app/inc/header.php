25052a7506b7c9a14285ad03011c1c51064e3d4d
Temak <makindeoluwatimilehin@gmail.com>
2020-09-05 01:36:55 +0100

    responsive fixes

--- blog-app/inc/header.php
+++ blog-app/inc/header.php
@@ -3,6 +3,11 @@
 <head>
 	<title>Basic PHP Blog App with CRUD functionalities</title>
 	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/cyborg/bootstrap.min.css">
+	<style type="text/css">
+		@media (min-width: 1024px){
+			.card{max-width: 350px;}
+		}
+	</style>
 </head>
 <body>
 <?php include('nav.php'); ?>
--- blog-app/index.php
+++ blog-app/index.php
@@ -22,9 +22,9 @@
 <?php require('inc/header.php'); ?>
 	<div class="container text-center my-5">
 		<h1>Posts</h1>
-		<div class="row d-flex justify-content-between justify-content-lg-center pt-3">
+		<div class="row d-flex justify-content-between justify-content-lg-center mx-3 mx-lg-0 pt-3">
 			<?php foreach($posts as $post) : ?>
-				<div class="col-12 col-lg-4 card text-white bg-primary px-0 mb-3 mx-lg-2" style="max-width: 350px;">
+				<div class="col-12 col-lg-4 card text-white bg-primary px-0 mb-3 mx-lg-2">
 				  <div class="card-header">Created on <?php echo $post['created_at']; ?> by <span class="font-weight-bold"><?php echo $post['author']; ?></span></div>
 				  <div class="card-body">
 				    <h2 class="card-title text-capitalize"><a class="text-white nav-link" href="post.php?id=<?php echo $post['id'] ?>"><?php echo $post['title']; ?></a></h2>
